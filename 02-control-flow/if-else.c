300c1c65df53db1ea652083abe0f5a8428b89dad
Aman Shukla <109803350+amanshukla87@users.noreply.github.com>
2026-09-03 23:58:24 +0530

    Add if else control flow example

---
+++ 02-control-flow/if-else.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if (number > 0)
+    {
+        printf("The number is positive.\n");
+    }
+    else if (number < 0)
+    {
+        printf("The number is negative.\n");
+    }
+    else
+    {
+        printf("The number is zero.\n");
+    }
+
+    if (number % 2 == 0)
+    {
+        printf("The number is even.\n");
+    }
+    else
+    {
+        printf("The number is odd.\n");
+    }
+
+    return 0;
+}
